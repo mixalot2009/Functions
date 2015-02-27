547c3cf74944d881036b562a22aca9004aaad49f
mixalot2009 <mixalot2009@gmail.com>
2015-02-26 22:22:38 -0500

    commit 5

--- js/script.js
+++ js/script.js
@@ -2,6 +2,14 @@
 
 var number = randomNumber(1,2);
 //defining the arguments
+var number2 = randomNumber2(1,2);
+//defining the arguments
+var number3 = randomNumber2(1,2);
+//defining the arguments
+var number4 = randomNumber2(1,2);
+//defining the arguments
+var number5 = randomNumber2(1,2);
+//defining the arguments
 function randomNumber(a,b){
     //defining the parameters
     numberGenerator = Math.floor((Math.random() * 100) + b + a);
@@ -9,4 +17,36 @@ function randomNumber(a,b){
     return numberGenerator;
     //returning code to argument.
 }
+function randomNumber2(a,b){
+    //defining the parameters
+    numberGenerator = Math.floor((Math.random() * 100) + b + a);
+    //code for generating a random number discovered on W3schools.com
+    return numberGenerator;
+    //returning code to argument.
+}
+function randomNumber3(a,b){
+    //defining the parameters
+    numberGenerator = Math.floor((Math.random() * 100) + b + a);
+    //code for generating a random number discovered on W3schools.com
+    return numberGenerator;
+    //returning code to argument.
+}
+function randomNumber4(a,b){
+    //defining the parameters
+    numberGenerator = Math.floor((Math.random() * 100) + b + a);
+    //code for generating a random number discovered on W3schools.com
+    return numberGenerator;
+    //returning code to argument.
+}
+function randomNumber5(a,b){
+    //defining the parameters
+    numberGenerator = Math.floor((Math.random() * 100) + b + a);
+    //code for generating a random number discovered on W3schools.com
+    return numberGenerator;
+    //returning code to argument.
+}
 console.log(number)
+console.log(number2)
+console.log(number3)
+console.log(number4)
+console.log(number5)
