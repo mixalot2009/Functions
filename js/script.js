7e549ce141c84f749fa677ddb7c58ea725dbe3bd
mixalot2009 <mixalot2009@gmail.com>
2015-02-26 22:17:58 -0500

    commit 4

--- js/script.js
+++ js/script.js
@@ -1,10 +1,12 @@
 //Christofer Mix 2/26/15 Functions Assignment
 
 var number = randomNumber(1,2);
-//defining the arguements
+//defining the arguments
 function randomNumber(a,b){
-    //defining the peramiters
+    //defining the parameters
     numberGenerator = Math.floor((Math.random() * 100) + b + a);
     //code for generating a random number discovered on W3schools.com
     return numberGenerator;
+    //returning code to argument.
 }
+console.log(number)
