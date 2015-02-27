ad1ec049cea88326abeb49c51419334de5af762d
mixalot2009 <mixalot2009@gmail.com>
2015-02-26 22:12:40 -0500

    commit 2

--- js/script.js
+++ js/script.js
@@ -1,6 +1,8 @@
 //Christofer Mix 2/26/15 Functions Assignment
-randomNumber(1,2)
 
-function randomNumber(){
+randomNumber(1,2);
 
+function randomNumber(a,b){
+    numberGenerator = Math.floor((Math.random() * 100) + b + a);
+    console.log(numberGenerator)
 }
